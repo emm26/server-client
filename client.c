11eacd4f047f8513e71fbfa479a6b7cb97fbd64e
Edgar Moreno <emm7121997@gmail.com>
2019-04-18 19:48:33 +0200

    minor refactoring

--- client.c
+++ client.c
@@ -106,8 +106,8 @@ void setup_TCP_socket();
 void setup_UDP_socket();
 void signup_on_server();
 
-/* input: ./myclient -c <software_config_file> -d
-          -f <network_dev_config_file>       */
+/* input: ./myclient {-d} {-c <software_config_file>}
+           {-f <network_dev_config_file> }             */
 int main(int argc, const char *argv[]) {
     client_data.unsuccessful_signups = 0;
     /* set server random num as 0000000 initially, will be changed when getting
